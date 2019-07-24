7287e913b6483256a9bac6bd6379e648d496b308
polyndrom <polyndromdev@gmail.com>
2019-07-24 15:01:21 +1000

    some fix

--- engine/core/sprite.js
+++ engine/core/sprite.js
@@ -26,8 +26,8 @@ class Sprite{
 		console.log(this.pos);	
 		sprite.setAttribute("block_x",this.pos.block_x);
 		sprite.setAttribute("block_y", this.pos.block_y);
-		sprite.setAttribute("x", this.pos.x * FIELD_SIZE);
-		sprite.setAttribute("y", this.pos.y * FIELD_SIZE);
+		sprite.setAttribute("x", this.pos.block_x * FIELD_SIZE);
+		sprite.setAttribute("y", this.pos.block_y * FIELD_SIZE);
 		sprite.setAttribute("width", SPRITE_WIDTH);
 		sprite.setAttribute("height", SPRITE_HEIGHT);
 		sprite.setAttributeNS("http://www.w3.org/1999/xlink", "href", this.sprite_url);
