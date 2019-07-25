16fddab1e8fe1d1a3f14277d62c718cb6bfbf1a3
Anton <anton.bayka@gmail.com>
2019-07-25 15:33:15 +1000

    GameObject render

--- engine/engine.js
+++ engine/engine.js
@@ -2,8 +2,14 @@ const FIELD_SIZE = 16;
 const SPRITE_WIDTH = 20;
 const SPRITE_HEIGHT = 20;
 
-var id_timer;
-var images = new Image();
+var canvas = document.getElementById('game_field');
+var ctx = canvas.getContext('2d');
+
+function render_map(map_url) {
+	var map = new Image();
+	map.src = map_url;	
+	ctx.drawImage(map, 0, 0);
+}
 
 class Position{
 	constructor(x, y, block_x, block_y){
@@ -15,8 +21,7 @@ class Position{
 }
 
 class GameObject{
-	constructor(pos, sprite_url, game_field){
-		this.ctx = game_field.getContext("2d");
+	constructor(pos, sprite_url){
 		this.pos = pos;
 		this.sprite_url = sprite_url;
 	}
@@ -25,20 +30,44 @@ class GameObject{
 		this.pos.x += x;
 		this.pos.y += y;	
 	}
+
+	is_outmap(){
+		if (this.pos.x > canvas.width + SPRITE_WIDTH || this.pos.y > canvas.height + SPRITE_HEIGHT 
+			|| this.pos.x < 0 - SPRITE_WIDTH || this.pos.y < 0 - SPRITE_HEIGHT)
+			return true;
+		return false;
+	}
 	
 	render(image){
+		if (this.is_outmap())
+			return false;
+		render_map("maps/map_4.png");
 		image.src = this.sprite_url;
-		this.ctx.save();
-		this.ctx.fillRect(0, 0, ctx.width, ctx.height);
-		this.ctx.restore();
-		this.ctx.drawImage(image, this.pos.x, this.pos.y, SPRITE_WIDTH, SPRITE_HEIGHT);
+		ctx.save();
+		ctx.restore();
+		ctx.drawImage(image, this.pos.x, this.pos.y, SPRITE_WIDTH, SPRITE_HEIGHT);
+		return true;
 	}
+
 }
 
-(function draw_object(){
-	var game_field = document.getElementById("game_field");
-	let pos = new Position(0, 0, 0, 0);
-	var object = new GameObject(pos, "sprites/towerDefense_tile270.png", game_field, 10, 10, 10);
-	object.translate_pos(4, 0);
-	id_timer = setInterval(function(){object.translate_pos(1, 0); object.render(images)}, 75);
-})()
+class MapManager{
+	constructor(static_image, source_str) {
+		this.static_image = static_image;
+		this.width = WIDTH;
+		this.height = HEIGHT;
+		this.map_container = new Array(this.height);
+		for(let i = 0; i < WIDTH; i++) {
+			this.map_container[i] = new Array(this.width);
+			for(let j = 0; j < HEIGHT; j++) {
+				this.map_container[i][j] = CHAR_TO_SPRITE[source_str[i][j]];
+			}
+		}
+	}
+}	
+
+render_map("maps/map_4.png");
+var image = new Image();
+var pos = new Position(0, 20, 0, 0);
+var object = new GameObject(pos, "sprites/towerDefense_tile271.png");
+var id_timer = setInterval(function(){object.translate_pos(1, 0); object.render(image, ctx, 1, 0)}, 16)
