57d709789522ecf33731fab9aae1fb6de74d510f
Anton <anton.bayka@gmail.com>
2019-07-26 02:04:56 +1000

    refactore grafics

--- engine/engine.js
+++ engine/engine.js
@@ -1,29 +1,27 @@
-const FIELD_SIZE = 16;
-const SPRITE_WIDTH = 20;
-const SPRITE_HEIGHT = 20;
- 
-class ObjectPosition{
-    constructor(x, y){
-        this.x = x;
-        this.y = y;
-    }
-}
- 
-class GameObject{
-    constructor(pos, image){
-        this.pos = pos;
-        this.image = image;
-    }
- 
-    translate_pos(x, y) {
-        this.pos.x += x;
-        this.pos.y += y;   
-    }
- 
-    is_outmap(){
-        if (this.pos.x >= canvas.width + SPRITE_WIDTH || this.pos.y >= canvas.height + SPRITE_HEIGHT
-            || this.pos.x <= 0 - SPRITE_WIDTH || this.pos.y <= 0 - SPRITE_HEIGHT)
-            return true;
-        return false;
-    }
+class Render {
+	constructor(canvas) {
+		this.canvas = canvas;
+		this.ctx = canvas.getContext('2d');
+		this.map = new Image();
+	}
+
+	set_map(map_url) {
+		this.map.src = map_url;	
+	}
+
+	render_map() {
+		this.ctx.drawImage(this.map, 0, 0); 	
+	}
+
+	render_objects(objects) {	
+		this.ctx.save();	
+		for (let i = 0; i < objects.length; ++i){
+			if (objects[i].is_outmap()){
+				objects.splice(i, 1);
+				continue;
+			}
+			this.ctx.drawImage(objects[i].image, objects[i].pos.x, objects[i].pos.y, SPRITE_WIDTH, SPRITE_HEIGHT);
+		}
+		this.ctx.restore();
+	}
 }
--- engine/game_object.js
+++ engine/game_object.js
@@ -1,35 +1,34 @@
 const SPRITE_WIDTH = 20;
 const SPRITE_HEIGHT = 20;
 const BLOCK_OFFSET = 20;
+ 
+class ObjectPosition {
+    constructor(x, y){
+        this.x = x;
+        this.y = y;
+    }
 
-class ObjectPosition{
-	constructor(x, y){
-		this.x = x;
-		this.y = y;
-	}
-
-	set_pos(x, y){
-		this.x = x;
-		this.y = y;
-	}
-
-	move_pos(x, y){
-		this.x += x;
-		this.y += y;
-	}
+    translate_pos(x, y) {
+        this.x += x;
+        this.y += y;   
+    }
 }
+ 
+class GameObject {
+    constructor(pos, image_url){
+        this.pos = pos;
+        this.image = new Image();
+        this.image.src = image_url;
+    }
 
-class GameObject{
-	constructor(pos, image_url){
-		this.pos = pos;
-		this.image = new Image();
-		this.image.src = image_url;
-	}
-
-	is_outmap(){
-		if (this.pos.x >= canvas.width + SPRITE_WIDTH || this.pos.y >= canvas.height + SPRITE_HEIGHT 
-			|| this.pos.x <= 0 - SPRITE_WIDTH || this.pos.y <= 0 - SPRITE_HEIGHT)
-			return true;
-		return false;
-	}
+    move(x, y) {
+    	this.pos.translate_pos(x, y);
+    }    
+ 
+    is_outmap() {
+        if (this.pos.x >= canvas.width + SPRITE_WIDTH || this.pos.y >= canvas.height + SPRITE_HEIGHT
+            || this.pos.x <= 0 - SPRITE_WIDTH || this.pos.y <= 0 - SPRITE_HEIGHT)
+            return true;
+        return false;
+    }
 }
