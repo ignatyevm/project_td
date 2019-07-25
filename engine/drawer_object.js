b795121e0028d06eab2bf8b2f62c920644ee641c
tinanator <43900415+tinanator@users.noreply.github.com>
2019-07-25 10:56:08 +1000

    Add files via upload

--- engine/drawer_object.js
+++ engine/drawer_object.js
@@ -1,19 +1,12 @@
 class Drawer{
 	constuctor(game_field){
 		this.element = document.getElementById(game_field);
-	}
-	move(pos, image){
-		let e = document.createElementNS("http://www.w3.org/2000/svg", "image");
-	    e.setAttributeNS("http://www.w3.org/1999/xlink", "href", image.src);
-	    e.setAttributeNS(null,"x", pos.x);
-	    e.setAttributeNS(null,"y", pos.y);
-	    e.setAttributeNS(null,"width", width);
-	    e.setAttributeNS(null,"height", height);
-	  	this.element.appendChild(e);
+		this.ctx = null;
 	}
 	clear(){
-		this.element.innerHTML = "";
+		this.ctx.clearRect(0, 0, this.element.width, this.element.height);
 	}
-
-
+	draw(image, x, y, width, height){
+		ctx.drawImage(image, x, y, width, height);
+	}
 }
