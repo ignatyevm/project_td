2a39390e4151220dc227c3bd7c72bc3ee43a7e72
tinanator <43900415+tinanator@users.noreply.github.com>
2019-07-26 23:25:30 +1000

    refactoring

--- engine/UI.js
+++ engine/UI.js
@@ -1,50 +1,42 @@
-var is_tower_chosen = false;
-var tower;
-
-
-let testcanvas = document.getElementById('testCanvas');
-
-let canvas = document.getElementById('game_field');
-let ctx = testcanvas.getContext('2d');
-
-let bX = 0;
-let bY = 0;
-let tower_y = 0;
-let tower_x = 0; 
-
-let updated_map = new CreateMap(1);
-let map = updated_map;
-
-let new_session = new Session(1);
-
-class testTower{
+class Tower{
 	constructor(x, y){
 		this.x = x;
 		this.y = y;
 		this.width = SPRITE_WIDTH;
 		this.height = SPRITE_HEIGHT;
+		this.radius = RADIUS;
+		this.selected = false;
 	}
 	draw(ctx){
-		ctx.fillStyle = 'blue';
-		ctx.fillRect(this.x, this.y, this.width, this.height);
+		let image = new Image()
+		image.src = 'sprites/towerDefense_tile291.png'
+		ctx.drawImage(image, this.x, this.y, this.width, this.height);
+		if (this.selected)
+			this.draw_circle(ctx);
+	}
+	draw_circle(ctx){
+		ctx.beginPath();
+		ctx.arc(this.x + this.width / 2, this.y + this.height / 2, this.radius, 0, 2 * Math.PI);
+		ctx.closePath();
+		ctx.stroke();
 	}
 }
 
-testcanvas.addEventListener("mousemove", function(event) {
+canvas.addEventListener("mousemove", function(event) {
+	let x = event.clientX;
+ 	let y = event.clientY;
 	if (is_tower_chosen){
-		let x = event.clientX;
-	 	let y = event.clientY;
 		bX = Math.floor(x / SPRITE_WIDTH);
 		bY = Math.floor(y / SPRITE_HEIGHT);
 		tower_x = SPRITE_WIDTH * bX;
 		tower_y = SPRITE_HEIGHT * bY;
 	}
-
+	for (let t of towers){
+		t.selected =
+			Math.abs(t.x + t.width / 2 - x) <= t.width &&
+			Math.abs(t.y + t.height / 2 - y) <= t.height;
+	}
 });
-
-
-
-towers = [];
 
 function set_tower(map, y, x){
 	let new_field = [];
@@ -61,27 +53,22 @@ function set_tower(map, y, x){
 	return new_field;
 }
 
-testcanvas.addEventListener("click", function(event){
+canvas.addEventListener("click", function(event){ 
 	if (is_tower_chosen){
 		if (map[bY][bX] == CHAR_FILL){
 			map = set_tower(map, bY, bX);
-			//map[bY][bX] = "T";
 			towers.push(new testTower(tower_x, tower_y));
-			new_session.objects.push(new GameObject("towerDefense_tile291.png", new Position(tower_x, tower_y)));
 			is_tower_chosen = false;
 		}
-		
 	}
-	
 });
 
 foo = function(){
+	ctx.clearRect(0, 0, 720, 720);
 	if (map[bY][bX] == CHAR_FILL){
-		ctx.clearRect(0, 0, 720, 720);
 		ctx.fillStyle = 'green';
 		ctx.fillRect(tower_x, tower_y, SPRITE_WIDTH, SPRITE_HEIGHT);
 	}
-	
 	for (t of towers){
 		t.draw(ctx);
 	}
