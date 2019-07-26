7a6d1668ed336c8ac423d29c6e0d9802d7026403
polyndrom <polyndromdev@gmail.com>
2019-07-27 01:25:31 +1000

    small fix & refactoring

--- engine/game_object.js
+++ engine/game_object.js
@@ -3,6 +3,8 @@ class GameObject{
 	constructor(x, y, drawer){
 		this.x = x;
 		this.y = y;
+		this.width = SPRITE_WIDTH;
+		this.height = SPRITE_HEIGHT;
 		this.drawer = drawer;
 	}
 
@@ -11,20 +13,27 @@ class GameObject{
 		let local_drawer = this.drawer;
 		let local_x = this.x;
 		let local_y = this.y;
+		let local_width = this.width;
+		let local_height = this.height;
 		let local_sprite = new Image();
 
 		this.sprite = local_sprite;
 
 		this.sprite.onload = function(){
-			local_drawer.render(local_sprite, local_x, local_y, SPRITE_WIDTH, SPRITE_HEIGHT);
+			local_drawer.render(local_sprite, local_x, local_y, local_width, local_height);
 		};
 
 		this.sprite.src = sprite_link;
 		
 	}
 
+	resize(width, height){
+		this.width = width;
+		this.height = height;
+	}
+
 	render(){
-		this.drawer.render(this.sprite, this.x, this.y, SPRITE_WIDTH, SPRITE_HEIGHT);
+		this.drawer.render(this.sprite, this.x, this.y, this.width, this.height);
 	}
 
 }
