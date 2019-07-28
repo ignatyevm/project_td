b453ef4396cee832005188f887df90882e710061
Grogse <timofeev.anik@students.dvfu.ru>
2019-07-28 18:30:45 +0900

    let

--- engine/math_helper.js
+++ engine/math_helper.js
@@ -17,12 +17,12 @@ function get_distance(point1, point2) {
 }
 
 function intersection_square(square, point) {
-	intersection_1 = (square[1][0] - square[0][0]) * (point[1] - square[0][1]) - (square[1][1] - square[0][1]) * (point[0] - square[0][0]);
-	intersection_2 = (square[2][0] - square[1][0]) * (point[1] - square[1][1]) - (square[2][1] - square[1][1]) * (point[0] - square[1][0]);
-	intersection_3 = (square[3][0] - square[2][0]) * (point[1] - square[2][1]) - (square[3][1] - square[2][1]) * (point[0] - square[2][0]);
-	intersection_4 = (square[0][0] - square[3][0]) * (point[1] - square[3][1]) - (square[0][1] - square[3][1]) * (point[0] - square[3][0]);
+	let intersection_1 = (square[1][0] - square[0][0]) * (point[1] - square[0][1]) - (square[1][1] - square[0][1]) * (point[0] - square[0][0]);
+	let intersection_2 = (square[2][0] - square[1][0]) * (point[1] - square[1][1]) - (square[2][1] - square[1][1]) * (point[0] - square[1][0]);
+	let intersection_3 = (square[3][0] - square[2][0]) * (point[1] - square[2][1]) - (square[3][1] - square[2][1]) * (point[0] - square[2][0]);
+	let intersection_4 = (square[0][0] - square[3][0]) * (point[1] - square[3][1]) - (square[0][1] - square[3][1]) * (point[0] - square[3][0]);
 
-	if (intersection_1 > 0 && intersection_2 > 0 && intersection_3 > 0 && intersection_4 > 0) return true;
+	if ((intersection_1 > 0 && intersection_2 > 0 && intersection_3 > 0 && intersection_4 > 0) || (intersection_1 > 0 && intersection_2 > 0 && intersection_3 > 0 && intersection_4 > 0)) return true;
 
 	return false;
 }
