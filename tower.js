01a6f59de2d189e3c7acae9cb1f32671072704a8
polyndrom <polyndromdev@gmail.com>
2019-07-28 16:18:16 +1000

    add fire method & something

--- tower.js
+++ tower.js
@@ -5,6 +5,7 @@
 class Tower extends GameObject{
 	
 	constructor(x, y, radius, drawer, meta_drawer) {
+
 		super(x, y, drawer);
 
 		this.x = x;
@@ -13,11 +14,51 @@ class Tower extends GameObject{
 		this.width = SPRITE_WIDTH;
 		this.height = SPRITE_HEIGHT;
 
+		this.drawer = drawer;
 		this.meta_drawer = meta_drawer;
 
 		this.radius = radius;
 
 		this.selected = true;
+
+
+		this.bullets = [];
+
+		// 50 - 1s
+		// 100 - 2s
+		// 25 - 05.s
+		this.max_fire_rate = 1;
+		this.current_fire_rate = 0;
+
+		this.targets_set = [];
+		this.targets_queue = [];
+
+	}
+
+	set_damage(damge){
+		this.damage = damage;
+	}
+
+	set_fire_rate(fire_rate){
+		this.max_fire_rate = fire_rate * 50;
+	}
+
+	update_bullets(){
+		for(let bullet of this.bullets){
+			bullet.render();
+			bullet.update_motion();
+		}
+	}
+
+	fire(target){
+		if(this.current_fire_rate % this.max_fire_rate == 0){
+			let bullet = new Bullet(this, target, this.drawer);
+			bullet.set_sprite("sprites/towerDefense_tile295.png");
+			bullet.set_speed(7);
+			this.bullets.push(bullet);
+			this.current_fire_rate = 0;
+		}
+		this.current_fire_rate++;
 	}
 
 	render(){
