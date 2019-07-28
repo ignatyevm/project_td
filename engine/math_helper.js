62c12f5bd8b05652226651ccbe00e47448fe13f5
Grogse <timofeev.anik@students.dvfu.ru>
2019-07-28 18:21:34 +0900

    null

--- engine/math_helper.js
+++ engine/math_helper.js
@@ -20,7 +20,7 @@ function intersection_square(square, point) {
 	intersection_1 = (square[1][0] - square[0][0]) * (point[1] - square[0][1]) - (square[1][1] - square[0][1]) * (point[0] - square[0][0]);
 	intersection_2 = (square[2][0] - square[1][0]) * (point[1] - square[1][1]) - (square[2][1] - square[1][1]) * (point[0] - square[1][0]);
 	intersection_3 = (square[3][0] - square[2][0]) * (point[1] - square[2][1]) - (square[3][1] - square[2][1]) * (point[0] - square[2][0]);
-	intersection_4 = (square[4][0] - square[3][0]) * (point[1] - square[3][1]) - (square[4][1] - square[3][1]) * (point[0] - square[3][0]);
+	intersection_4 = (square[0][0] - square[3][0]) * (point[1] - square[3][1]) - (square[0][1] - square[3][1]) * (point[0] - square[3][0]);
 
 	if (intersection_1 > 0 && intersection_2 > 0 && intersection_3 > 0 && intersection_4 > 0) return true;
 
